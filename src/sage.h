d61df3501082a81e47e02abc3fbc9351f4c3cb85
Abhishek Chakravarti <abhishek@taranjali.org>
2019-11-17 11:22:11 +0530

    define SAGE_PURE compiler hint
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ src/sage.h
@@ -0,0 +1,28 @@
+#if (defined __GNUC__ || defined __clang__)
+#   pragma once
+#else
+#   if (!defined SCHEME_ASSISTED_GAME_ENGINE)
+#       define SCHEME_ASSISTED_GAME_ENGINE
+#endif
+
+#if defined __cplusplus
+    extern "C" {
+#endif
+
+
+#if (defined __GNUC__ || defined __clang__)
+#   define SAGE_PURE __attribute__((pure))
+#else
+#   define SAGE_PURE
+#   warning SAGE_PURE has no effect on non GCC-compatible compilers
+#endif
+
+
+#if defined __cplusplus
+    } /* extern "C" */
+#endif
+
+#if !(defined __GNUC__ || defined __clang__)
+#   endif /* SCHEME_ASSISTED_GAME_ENGINE */
+#endif
+
