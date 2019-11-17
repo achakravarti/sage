b6519e21a93302d60a290131413b68d7bb0337fe
Abhishek Chakravarti <abhishek@taranjali.org>
2019-11-17 13:40:15 +0530

    Define sage_compiler_gnuex() macro
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/sage.h
+++ src/sage.h
@@ -13,7 +13,8 @@
 #define SAGE_COMPILER_GCC (0)
 #define SAGE_COMPILER_CLANG (1)
 
-#if (defined __GNUC__)
+/* https://stackoverflow.com/questions/38499462/ */
+#if (defined __GNUC__ && !defined __clang__)
 #   define sage_compiler() SAGE_COMPILER_GCC
 #elif (defined __clang__)
 #   define sage_compiler() SAGE_COMPILER_CLANG
@@ -22,15 +23,20 @@
 #endif
 
 
-#if (defined __GNUC__ || defined __clang__)
+#define sage_compiler_gnuex()              \
+    (sage_compiler () == SAGE_COMPILER_GCC \
+     || sage_compiler == SAGE_COMPILER_CLANG)
+
+
+#if (sage_compiler_gnuex ())
 #   define SAGE_PURE __attribute__((pure))
 #else
-#   define SAGE_PURE
-#   warning "SAGE_PURE has no effect on non GCC-compatible compilers"
+#   define SAGE_PURE 
+#   warning "SAGE_PURE has no effect on non GCC-compatible compilers" 
 #endif
 
 
-#if (defined __GNUC__ || defined __clang__)
+#if (sage_compiler_gnuex ())
 #   define SAGE_HOT __attribute__((hot))
 #else
 #   define SAGE_HOT
@@ -38,7 +44,7 @@
 #endif
 
 
-#if (defined __GNUC__ || defined __clang__)
+#if (sage_compiler_gnuex ())
 #   define SAGE_COLD __attribute__((cold))
 #else
 #   define SAGE_COLD
@@ -46,7 +52,7 @@
 #endif
 
 
-#if (defined __GNUC__ || defined __clang__)
+#if (sage_compiler_gnuex ())
 #   define sage_likely(p) (__builtin_expect (!!(p), 1))
 #else
 #   define sage_likely(p) (p)
@@ -54,7 +60,7 @@
 #endif
 
 
-#if (defined __GNUC__ || defined __clang__)
+#if (sage_compiler_gnuex ())
 #   define sage_unlikely(p) (__builtin_expect (!!(p), 0))
 #else
 #   define sage_unlikely(p) (p)
